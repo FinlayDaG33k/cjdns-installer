0ac3916f74a76cc063c7937f740b8ffe8538fbac
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-05-23 11:54:31 +0200

    Update ez-install.sh

--- ez-install.sh
+++ ez-install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+#CjDNS Ez-Installer by FinlayDaG33k and Ayekira
+
 wget https://raw.githubusercontent.com/FinlayDaG33k/cjdns-installer/master/cjdns.sh -O /etc/init.d/cjdns
 chmod +x /etc/init.d/cjdns
 sudo apt-get update
